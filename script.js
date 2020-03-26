31c03f9a02ad5b59f54d1f45eb22480cea3fadf3
IBOYI WISDOM EMMANUEL <62144758+wisdomiboyi@users.noreply.github.com>
2020-03-26 23:54:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+cont name="Wisdom Emmanuel Iboyi";
+cont courses = ["HTML", "CSS", "Javascript"];
+console.log('my name is '+ name + '& my courses are' + courses);
+
+const oddCourses=[]
+for (let i=0; i<=200; i++){
+if(i%2===0) 
+{oddCourses.push(i);}
+}
+console.log(oddCourses);
